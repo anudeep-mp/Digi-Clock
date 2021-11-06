ec98a2a675acc0b1b7c43fc7a4acb69a99e8449f
Anudeep Muktinutalapati <anudeep.mp@yahoo.com>
2021-11-06 09:17:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,66 @@
-"use strict";
+'use strict'
 
 function currentTime() {
-  var date = new Date();
-  var day = date.getDay();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
-  var month = date.getMonth();
-  var curr_date = date.getDate();
-  var year = date.getFullYear();
+  var date = new Date()
+  var day = date.getDay()
+  var hour = date.getHours()
+  var min = date.getMinutes()
+  var sec = date.getSeconds()
+  var month = date.getMonth()
+  var curr_date = date.getDate()
+  var year = date.getFullYear()
   var month_name = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  var showDay = document.querySelectorAll(".day_wrapper span");
-  var midday = "AM";
-  midday = hour >= 12 ? "PM" : "AM";
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  var showDay = document.querySelectorAll('.day_wrapper span')
+  var midday = 'AM'
+  midday = hour >= 12 ? 'PM' : 'AM'
 
-  hour = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
-  hour = updateTime(hour);
-  min = updateTime(min);
-  sec = updateTime(sec);
-  curr_date = updateTime(curr_date);
-  document.querySelector("#time").innerHTML = `${hour}:${min}`;
-  document.querySelector("#sec").innerHTML = `${sec}`;
-  document.querySelector("#med").innerHTML = `${midday}`;
+  hour = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour
+  hour = updateTime(hour)
+  min = updateTime(min)
+  sec = updateTime(sec)
+  curr_date = updateTime(curr_date)
+  document.querySelector('#time').innerHTML = `${hour}:${min}`
+  document.querySelector('#sec').innerHTML = `${sec}`
+  document.querySelector('#med').innerHTML = `${midday}`
   document.querySelector(
-    "#full_date"
-  ).innerHTML = `${month_name[month]} ${curr_date} ${year}`;
-  showDay[day].style.opacity = "1";
-  
-  var wishing = "MORNING";
+    '#full_date'
+  ).innerHTML = `${month_name[month]} ${curr_date} ${year}`
+  showDay[day].style.opacity = '1'
 
-  if (midday === "AM") {
-    if (hour < 4 || hour == 12) wishing = "NIGHT";
-    else wishing = "MORNING";
+  var wishing = 'MORNING'
+
+  if (midday === 'AM') {
+    if (hour < 4 || hour == 12) wishing = 'NIGHT'
+    else wishing = 'MORNING'
   } else {
-    if (hour < 4 || hour == 12) wishing = "AFTERNOON";
-    else wishing = "EVENING";
+    if (hour < 4 || hour == 12) wishing = 'AFTERNOON'
+    else wishing = 'EVENING'
   }
 
-  document.querySelector("#wish").innerHTML = `GOOD ${wishing} ANUDEEP`;
+  document.querySelector('#wish').innerHTML = `GOOD ${wishing} ANUDEEP`
+  document.querySelector('#page-title').innerHTML = `Good ${
+    wishing.charAt(0).toUpperCase() + wishing.substr(1).toLowerCase()
+  } | ${hour}:${min}:${sec} ${midday}`
 }
 function updateTime(k) {
   if (k < 10) {
-    return "0" + k;
+    return '0' + k
   } else {
-    return k;
+    return k
   }
 }
 
-setInterval(currentTime, 1000);
+setInterval(currentTime, 1000)
